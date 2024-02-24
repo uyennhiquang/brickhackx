8644d5a2bec968b0f99b285973eebe4a00e9c97f
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2024-02-24 18:05:27 -0500

    Assign each squareElt with row/col

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,29 @@ import "../assets/styles/styles.css";
 
 import { Domineering } from "./domineering/model/domineering";
 
-const model = new Domineering("vplayer", "hplayer");
+let model;
 
 const startBtnElt = document.getElementById("button--start");
 const menuDisplayElt = document.getElementById("display--menu");
 const gameDisplayElt = document.getElementById("display--game");
 
+const tBodyEl = document.getElementById("table--game").firstElementChild;
+console.log(tBodyEl);
+for (let row = 0; row < Domineering.MAX_ROWS; row++) {
+  const currentRow = tBodyEl.children[row];
+  currentRow.setAttribute("data-row", String(row));
+  for (let col = 0; col < Domineering.MAX_COLS; col++) {
+    const currentTD = currentRow.children[col];
+    currentTD.setAttribute("data-col", String(col));
+  }
+}
+
 startBtnElt.addEventListener("click", () => {
+  const vPlayerName = (<HTMLInputElement>document.getElementById("v")).value;
+  const hPlayerName = (<HTMLInputElement>document.getElementById("h")).value;
+
+  model = new Domineering(vPlayerName, hPlayerName);
+
   menuDisplayElt.classList.toggle("hidden");
   gameDisplayElt.classList.toggle("hidden");
 });

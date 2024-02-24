7f41cf69db1e30abcf02a55c5ea75fdc46385f7d
uyennhiquang <111651301+uyennhiquang@users.noreply.github.com>
2024-02-24 17:37:33 -0500

    Refactor html and css structure

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,13 @@ import { Domineering } from "./domineering/model/domineering";
 const model = new Domineering("vplayer", "hplayer");
 
 
+const startBtnElt = document.getElementById("button--start");
+const menuDisplayElt = document.getElementById("display--menu");
+const gameDisplayElt = document.getElementById("display--game");
+
+startBtnElt.addEventListener("click", () => {
+  // menuDisplayElt.classList.toggle("hidden");
+  gameDisplayElt.classList.toggle("hidden");
+});
+
+// console.log(menuDisplayElt);
